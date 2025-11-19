56af6df9ad12014c447eb71d15a8d209e00ad0f7
Alex Bui <atb2923@cs.utexas.edu>
2025-11-19 16:25:35 -0600

    final

--- trie.c
+++ trie.c
@@ -87,10 +87,12 @@ bool insert(trie_node_t* root, char* word) {
  * @param root the root of the Trie to clean up
  */
 void free_trie(trie_node_t* root) {
-    free_trie_node(&root);
     for (int i = 0; i < ASCII_CHARS - FIRST_VALID_CHAR; i++) {
-        free_trie(root->children[i]);
+        if (root->children[i]) {
+            free_trie(root->children[i]);
+        }
     }
+    free_trie_node(&root);
 }
 
 static void free_trie_node(trie_node_t** node) {
